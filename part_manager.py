74cb354a2707c0bbea17e1dfcadd86b17ed76be8
Ben Chavez <ben.m.chavez@gmail.com>
2020-03-23 16:14:25 +0100

    finished the gui application

--- part_manager.py
+++ part_manager.py
@@ -1,4 +1,56 @@
 from Tkinter import *
+import tkMessageBox
+from db import Database
+
+db = Database('store.db')
+
+def populate_list():
+    parts_list.delete(0, END)
+    for row in db.fetch():
+        parts_list.insert(END, row)
+
+def add_item():
+    if part_text.get() == '' or customer_text.get() =='' or retailer_text.get == '' or price_text.get =='':
+        tkMessageBox.showerror('Required Fields', 'Please include all fields')
+        return
+    db.insert(part_text.get(), customer_text.get(), retailer_text.get(), price_text.get())
+    parts_list.delete(0, END)
+    parts_list.insert(END, (part_text.get(), customer_text.get(), retailer_text.get(), price_text.get()))
+    clear_text()
+    populate_list()
+
+def select_item(event):
+    try:
+        global selected_item
+        index = parts_list.curselection()[0]
+        selected_item = parts_list.get(index)
+        
+        part_entry.delete(0, END)
+        part_entry.insert(END, selected_item[1])
+        customer_entry.delete(0, END)
+        customer_entry.insert(END, selected_item[2])
+        retailer_entry.delete(0, END)
+        retailer_entry.insert(END, selected_item[3])
+        price_entry.delete(0, END)
+        price_entry.insert(END, selected_item[4])
+    except IndexError:
+        pass   
+
+def remove_item():
+    db.remove(selected_item[0])
+    clear_text()
+    populate_list()
+
+def update_item():
+    db.update(selected_item[0], part_text.get(), customer_text.get(), retailer_text.get(), price_text.get())
+    clear_text()
+    populate_list()
+
+def clear_text():
+    part_entry.delete(0, END)
+    customer_entry.delete(0, END)
+    retailer_entry.delete(0, END)
+    price_entry.delete(0, END)
 
 # Create window object
 app = Tk()
@@ -39,9 +91,31 @@ parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
 scrollbar = Scrollbar(app)
 scrollbar.grid(row=3, column=3)
 
+# Set scroll to listbox
+parts_list.configure(yscrollcommand=scrollbar.set)
+scrollbar.configure(command=parts_list.yview)
+
+# Bind select
+parts_list.bind('<<ListboxSelect>>', select_item)
+
+# Buttons
+add_btn = Button(app, text="Add Part", width=12, command=add_item)
+add_btn.grid(row=2, column=0, pady=20)
+
+remove_btn = Button(app, text="Remove Part", width=12, command=remove_item)
+remove_btn.grid(row=2, column=1)
+
+update_btn = Button(app, text="Update Part", width=12, command=update_item)
+update_btn.grid(row=2, column=2)
+
+clear_btn = Button(app, text="Clear Input", width=12, command=clear_text)
+clear_btn.grid(row=2, column=3)
 
 app.title('Part Manager')
 app.geometry('800x350')
+
+# Populate data
+populate_list()
 
 # Start Program
 app.mainloop()
